a0805ca54e2e37cc9442dabbbb03cfbbc3a63f87
Mahdi Hasan <mahdihasan10599@gmail.com>
2023-05-21 16:46:56 +0600

    Created 404 page and added animated 404 image using lottiefiles json

--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -1,10 +1,44 @@
-import React from 'react';
+import React, { forwardRef, useEffect, useRef } from 'react';
+import Lottie from 'lottie-react';
+import errorImage from '../../../public/error.json';
+import { Link, useRouteError } from 'react-router-dom';
 
-const ErrorPage = () => {
+
+
+
+const LottieAnimation = forwardRef((props, ref) => {
+    const animationRef = useRef(null);
+
+    useEffect(() => {
+        if (animationRef.current) {
+            animationRef.current.play();
+        }
+    }, []);
+
     return (
-        <div>
-            
+        <Lottie
+            animationData={errorImage}
+            loop
+            autoplay
+            style={{ width: 450, height: 450 }}
+            lottieRef={ref || animationRef}
+        />
+    );
+});
+const ErrorPage = () => {
+    const animationRef = useRef(null);
+    const routerError = useRouteError();
+    const {status, statusText} = routerError;
+
+    return (
+      <section>
+        <div className='flex items-center justify-center pt-10 pb-5'>
+            <LottieAnimation ref={animationRef} />
         </div>
+        <div className='text-center pb-5'>
+            <Link className='py-2 px-6 bg-[#fd766f] text-white' to='/'>Back to home</Link>
+        </div>
+      </section>
     );
 };
 
