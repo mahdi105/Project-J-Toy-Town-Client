c2986afab4d124d3325698488eed25a5c39421c4
Mahdi Hasan <mahdihasan10599@gmail.com>
2023-05-21 23:30:34 +0600

    Created 2 event handler to load data from Database by Limit and name of each document

--- src/components/Pages/AllToys.jsx
+++ src/components/Pages/AllToys.jsx
@@ -1,10 +1,182 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { toast } from 'react-hot-toast';
+import { FaSearch } from "react-icons/fa";
 
+const notifyError = (error) => toast.error(error.message);
 const AllToys = () => {
+    const [toys, setToys] = useState([]);
+    const handleSearch = (event) => {
+        event.preventDefault();
+        const form = event.target;
+        const searchString = (form.search.value).toLowerCase();
+        fetch(`http://localhost:5000/toys?name=${searchString}`)
+        .then(res => res.json())
+        .then(data => setToys(data))
+        .catch(error => notifyError(error));
+    }
+    const loadToys = (limit) => {
+        useEffect(()=> {
+            fetch(`http://localhost:5000/toys?limit=${limit}`)
+            .then(res => res.json())
+            .then(data => setToys(data))
+            .catch(error => notifyError(error))
+        },[limit])
+    }
+    loadToys(20);
+    console.log(toys);
     return (
-        <div>
-            
-        </div>
+        <section className='container mx-auto px-2 md:px-0 lg:px-10'>
+            <div>
+                <form onSubmit={handleSearch}>
+                    <input type="text" name="search" id="" placeholder='Search by toy name' required />
+                    <button type='submit'><FaSearch/></button>
+                </form>
+            </div>
+            <div>
+                <div className="overflow-x-auto w-full">
+                    <table className="table w-full">
+                        {/* head */}
+                        <thead>
+                            <tr>
+                                <th>
+                                    <label>
+                                        <input type="checkbox" className="checkbox" />
+                                    </label>
+                                </th>
+                                <th>Name</th>
+                                <th>Job</th>
+                                <th>Favorite Color</th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {/* row 1 */}
+                            <tr>
+                                <th>
+                                    <label>
+                                        <input type="checkbox" className="checkbox" />
+                                    </label>
+                                </th>
+                                <td>
+                                    <div className="flex items-center space-x-3">
+                                        <div className="avatar">
+                                            <div className="mask mask-squircle w-12 h-12">
+                                                <img src="/tailwind-css-component-profile-2@56w.png" alt="Avatar Tailwind CSS Component" />
+                                            </div>
+                                        </div>
+                                        <div>
+                                            <div className="font-bold">Hart Hagerty</div>
+                                            <div className="text-sm opacity-50">United States</div>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>
+                                    Zemlak, Daniel and Leannon
+                                    <br />
+                                    <span className="badge badge-ghost badge-sm">Desktop Support Technician</span>
+                                </td>
+                                <td>Purple</td>
+                                <th>
+                                    <button className="btn btn-ghost btn-xs">details</button>
+                                </th>
+                            </tr>
+                            {/* row 2 */}
+                            <tr>
+                                <th>
+                                    <label>
+                                        <input type="checkbox" className="checkbox" />
+                                    </label>
+                                </th>
+                                <td>
+                                    <div className="flex items-center space-x-3">
+                                        <div className="avatar">
+                                            <div className="mask mask-squircle w-12 h-12">
+                                                <img src="/tailwind-css-component-profile-3@56w.png" alt="Avatar Tailwind CSS Component" />
+                                            </div>
+                                        </div>
+                                        <div>
+                                            <div className="font-bold">Brice Swyre</div>
+                                            <div className="text-sm opacity-50">China</div>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>
+                                    Carroll Group
+                                    <br />
+                                    <span className="badge badge-ghost badge-sm">Tax Accountant</span>
+                                </td>
+                                <td>Red</td>
+                                <th>
+                                    <button className="btn btn-ghost btn-xs">details</button>
+                                </th>
+                            </tr>
+                            {/* row 3 */}
+                            <tr>
+                                <th>
+                                    <label>
+                                        <input type="checkbox" className="checkbox" />
+                                    </label>
+                                </th>
+                                <td>
+                                    <div className="flex items-center space-x-3">
+                                        <div className="avatar">
+                                            <div className="mask mask-squircle w-12 h-12">
+                                                <img src="/tailwind-css-component-profile-4@56w.png" alt="Avatar Tailwind CSS Component" />
+                                            </div>
+                                        </div>
+                                        <div>
+                                            <div className="font-bold">Marjy Ferencz</div>
+                                            <div className="text-sm opacity-50">Russia</div>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>
+                                    Rowe-Schoen
+                                    <br />
+                                    <span className="badge badge-ghost badge-sm">Office Assistant I</span>
+                                </td>
+                                <td>Crimson</td>
+                                <th>
+                                    <button className="btn btn-ghost btn-xs">details</button>
+                                </th>
+                            </tr>
+                            {/* row 4 */}
+                            <tr>
+                                <th>
+                                    <label>
+                                        <input type="checkbox" className="checkbox" />
+                                    </label>
+                                </th>
+                                <td>
+                                    <div className="flex items-center space-x-3">
+                                        <div className="avatar">
+                                            <div className="mask mask-squircle w-12 h-12">
+                                                <img src="/tailwind-css-component-profile-5@56w.png" alt="Avatar Tailwind CSS Component" />
+                                            </div>
+                                        </div>
+                                        <div>
+                                            <div className="font-bold">Yancy Tear</div>
+                                            <div className="text-sm opacity-50">Brazil</div>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>
+                                    Wyman-Ledner
+                                    <br />
+                                    <span className="badge badge-ghost badge-sm">Community Outreach Specialist</span>
+                                </td>
+                                <td>Indigo</td>
+                                <th>
+                                    <button className="btn btn-ghost btn-xs">details</button>
+                                </th>
+                            </tr>
+                        </tbody>
+
+                    </table>
+                </div>
+            </div>
+
+        </section>
     );
 };
 
--- src/components/Utils/CategoryTab/ToyCardHome.jsx
+++ src/components/Utils/CategoryTab/ToyCardHome.jsx
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Rating } from '@smastrom/react-rating';
 import { Link } from 'react-router-dom';
+import { authContext } from '../../../AuthProvider/AuthProvider';
+import { toast } from 'react-hot-toast';
 
+const notify = () => toast.error('You have to login first to view details')
 const ToyCardHome = ({ toy }) => {
+    const {user} = useContext(authContext);
     const { name, image, price, rating, _id } = toy;
     return (
         <div className="card bg-base-100 shadow-xl">
@@ -21,7 +25,7 @@ const ToyCardHome = ({ toy }) => {
                     />
                 </div>
                 <div className="card-actions justify-start">
-                    <Link to={`/toy/${_id}`}  className="py-2 px-4 rounded-full text-white bg-[#09CCD0] shadow-lg hover:bg-white border-2 transition-all duration-300 hover:border-[#09CCD0] hover:text-black text-[13px] md:text-[16px]">View details</Link>
+                    <Link onClick={user || notify} to={`/toy/${_id}`}  className="py-2 px-4 rounded-full text-white bg-[#09CCD0] shadow-lg hover:bg-white border-2 transition-all duration-300 hover:border-[#09CCD0] hover:text-black text-[13px] md:text-[16px]">View details</Link>
                 </div>
             </div>
         </div>
